500b56377c7682ef98fded2221c107c84b4058a1
姜凌瀚 <jianglinghan@hotmail.com>
2020-06-11 17:07:01 +0800

    Delete data.py

--- data.py
+++
@@ -1,4 +0,0 @@
-from utils import sort
-#sort('data/ppi/atpin.ppi.2evidence.no-weight.txt', 'data/arabi_gdic.txt', out='test/test.ppi.txt')
-sort('data/ppi/atpin.ppi.2evidence.no-weight.txt', 'data/arabi_gdic.txt', out='data/ppi_mat/atpin.2.no-weight.ppi.txt')
-sort('data/ppi/atpin.ppi.2evidence.weight.txt', 'data/arabi_gdic.txt', out='data/ppi_mat/atpin.2.weight.ppi.txt')
